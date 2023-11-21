310e7f27a39943e6a699689282d4683c285f1288
juanlavieri <juan.lavieri@gmail.com>
2023-11-20 21:14:02 -0600

    Implement frontend logic for updating user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function UserProfile() {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [editName, setEditName] = useState('');
+    const [editEmail, setEditEmail] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:3001/api/users/profile', {
@@ -17,15 +20,47 @@ function UserProfile() {
     });
   }, []);
 
+  const handleEdit = () => {
+    setIsEditMode(true);
+    setEditName(user.name);
+    setEditEmail(user.email);
+  };
+
+  const handleSave = () => {
+    // Example: PUT request to '/api/users/profile'
+    axios.put('http://localhost:3001/api/users/profile', {
+      name: editName,
+      email: editEmail
+      // Add other fields as necessary
+    }, {
+      // Include authorization header if needed
+    })
+    .then(response => {
+      setUser(response.data);
+      setIsEditMode(false);
+    })
+    .catch(error => {
+      console.error('Error updating profile', error);
+    });
+  };
+
   return (
     <div>
       <h1>User Profile</h1>
       {user ? (
-        <div>
-          <p>Name: {user.name}</p>
-          <p>Email: {user.email}</p>
-          {/* Display additional user information here */}
-        </div>
+        isEditMode ? (
+          <div>
+            <input type="text" value={editName} onChange={e => setEditName(e.target.value)} />
+            <input type="email" value={editEmail} onChange={e => setEditEmail(e.target.value)} />
+            <button onClick={handleSave}>Save</button>
+          </div>
+        ) : (
+          <div>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <button onClick={handleEdit}>Edit</button>
+          </div>
+        )
       ) : (
         <p>Loading user profile...</p>
       )}
